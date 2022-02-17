2d868c7d46c9c0ae607aca163a5636deeed0b118
Procedurally-Generated-Human <ptoopchinezhad@gmail.com>
2022-02-17 19:28:19 +0330

    first stage of front done

--- shortner/urls.py
+++ shortner/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('create/<int:id>/', views.create, name="create"),
 ]
--- shortner/views.py
+++ shortner/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def home(request):
     return render(request, 'home.html')
+
+
+def create(request, id):
+    print("helllllloooooooo")
+    return redirect('home')
