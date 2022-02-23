9f4f84d5d2b12ac1696a493e515d3c1552813b7b
Procedurally-Generated-Human <ptoopchinezhad@gmail.com>
2022-02-23 17:50:56 +0330

    added hashing alog

--- shortner/forms.py
+++ shortner/forms.py
@@ -1,15 +1,18 @@
 from django import forms
 from .models import UrlLink
 
-import random
+import hashlib, base64
 
 class UrlForm(forms.ModelForm):
 
     url = forms.CharField(label='Your URL', max_length=1000)
 
-    def generate_short_url(self):
-        short_url = random.randint(1,20)
-        return short_url
+    def generate_short_url(self, text):
+        text_utf8 = text.encode('utf8')
+        md5 = hashlib.md5(text_utf8).digest()
+        b64 = base64.b64encode(md5)
+        result = b64.decode()[:10]
+        return result
 
     class Meta:
         model = UrlLink
--- shortner/views.py
+++ shortner/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, redirect
 from .forms import UrlForm
 
+
 def home(request):
     if request.method == "POST":
         form = UrlForm(request.POST)
         if form.is_valid():
             new_link = form.save(commit=False)
-            new_link.short_url = 5
             new_link.original_url = form.cleaned_data['url']
+            new_link.short_url = form.generate_short_url(new_link.original_url)
             new_link.save()
 
             return redirect('home')
